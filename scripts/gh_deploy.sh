6a34647ed01f89c48cb475f3536db2c28ecc7ea6
goodGid <hello.goodgid@gmail.com>
2022-01-30 23:48:51 +0900

    Update gh_deploy.sh

--- scripts/gh_deploy.sh
+++ scripts/gh_deploy.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
 PROJECT_NAME="github_action"
 JAR_PATH="/home/ubuntu/github_action/build/libs/*.jar"
+DEPLOY_PATH=/home/ubuntu/$PROJECT_NAME/
 DEPLOY_LOG_PATH="/home/ubuntu/$PROJECT_NAME/deploy.log"
 DEPLOY_ERR_LOG_PATH="/home/ubuntu/$PROJECT_NAME/deploy_err.log"
-
 BUILD_JAR=$(ls $JAR_PATH)
 JAR_NAME=$(basename $BUILD_JAR)
+
 echo "> build 파일명: $JAR_NAME" >> $DEPLOY_LOG_PATH
 
-echo "> build 파일 복사" >> $DEPLOY_LOG_PATH
-DEPLOY_PATH=/home/ubuntu/$PROJECT_NAME/
-cp $BUILD_JAR $DEPLOY_PATH
-
-echo "> 현재 실행중인 애플리케이션 pid 확인" >> $DEPLOY_LOG_PATH
+echo "> 현재 동작하는 어플리케이션 pid 체크" >> $DEPLOY_LOG_PATH
 CURRENT_PID=$(pgrep -f $JAR_NAME)
 
 if [ -z $CURRENT_PID ]
 then
-  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> $DEPLOY_LOG_PATH
+  echo "> 현재 동작하는 어플리케이션 존재 X" >> $DEPLOY_LOG_PATH
 else
-  echo "> kill -15 $CURRENT_PID"
+  echo "> 기존에 동작하는 어플리케이션 강제 종료 : kill -15 $CURRENT_PID" >> $DEPLOY_LOG_PATH
   kill -15 $CURRENT_PID
   sleep 5
 fi
