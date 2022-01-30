c925515bea79fd202b183725c18ad14d6317fdb3
goodGid <hello.goodgid@gmail.com>
2022-01-30 21:50:50 +0900

    Update gh_deploy.sh

--- scripts/gh_deploy.sh
+++ scripts/gh_deploy.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 PROJECT_NAME="github_action"
 JAR_PATH="/home/ubuntu/github_action/build/libs/*.jar"
+DEPLOY_LOG_PATH="/home/ubuntu/$PROJECT_NAME/deploy.log"
+DEPLOY_ERR_LOG_PATH="/home/ubuntu/$PROJECT_NAME/deploy_err.log"
 
 BUILD_JAR=$(ls $JAR_PATH)
 JAR_NAME=$(basename $BUILD_JAR)
-echo "> build 파일명: $JAR_NAME" >> /home/ubuntu/$PROJECT_NAME/deploy.log
+echo "> build 파일명: $JAR_NAME" >> $DEPLOY_LOG_PATH
 
-echo "> build 파일 복사" >> /home/ubuntu/$PROJECT_NAME/deploy.log
+echo "> build 파일 복사" >> $DEPLOY_LOG_PATH
 DEPLOY_PATH=/home/ubuntu/$PROJECT_NAME/
 cp $BUILD_JAR $DEPLOY_PATH
 
-echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ubuntu/$PROJECT_NAME/deploy.log
+echo "> 현재 실행중인 애플리케이션 pid 확인" >> $DEPLOY_LOG_PATH
 CURRENT_PID=$(pgrep -f $JAR_NAME)
 
 if [ -z $CURRENT_PID ]
 then
-  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ubuntu/$PROJECT_NAME/deploy.log
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> $DEPLOY_LOG_PATH
 else
   echo "> kill -15 $CURRENT_PID"
   kill -15 $CURRENT_PID
@@ -23,5 +25,5 @@ else
 fi
 
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
-echo "> DEPLOY_JAR 배포"    >> /home/ubuntu/$PROJECT_NAME/deploy.log
-nohup java -jar $DEPLOY_JAR >> /home/ubuntu/$PROJECT_NAME/deploy.log 2>/home/ubuntu/$PROJECT_NAME/deploy_err.log &
+echo "> DEPLOY_JAR 배포"    >> $DEPLOY_LOG_PATH
+nohup java -jar $DEPLOY_JAR >> $DEPLOY_LOG_PATH 2> $DEPLOY_ERR_LOG_PATH &
