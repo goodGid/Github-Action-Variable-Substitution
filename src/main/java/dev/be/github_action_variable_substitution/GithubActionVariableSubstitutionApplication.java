294b22f805e78cc0043a6de42bc94c6701526cb6
goodGid <hello.goodgid@gmail.com>
2022-01-30 20:45:23 +0900

    Init Commit

---
+++ src/main/java/dev/be/github_action_variable_substitution/GithubActionVariableSubstitutionApplication.java
@@ -0,0 +1,13 @@
+package dev.be.github_action_variable_substitution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionVariableSubstitutionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubActionVariableSubstitutionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/be/github_action_variable_substitution/GithubActionVariableSubstitutionApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.be.github_action_variable_substitution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionVariableSubstitutionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
