fa089fb1f955f6ed750926744dcc9682327237ca
goodGid <hello.goodgid@gmail.com>
2022-01-30 20:47:38 +0900

    Add `gh_deploy.sh` file

---
+++ scripts/gh_deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+PROJECT_NAME="github_action"
+JAR_PATH="/home/ubuntu/github_action/github-action-variable-substitution/build/libs/*.jar"
+
+BUILD_JAR=$(ls $JAR_PATH)
+JAR_NAME=$(basename $BUILD_JAR)
+echo "> build 파일명: $JAR_NAME" >> /home/ubuntu/$PROJECT_NAME/deploy.log
+
+echo "> build 파일 복사" >> /home/ubuntu/$PROJECT_NAME/deploy.log
+DEPLOY_PATH=/home/ubuntu/$PROJECT_NAME/
+cp $BUILD_JAR $DEPLOY_PATH
+
+echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ubuntu/$PROJECT_NAME/deploy.log
+CURRENT_PID=$(pgrep -f $JAR_NAME)
+
+if [ -z $CURRENT_PID ]
+then
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ubuntu/$PROJECT_NAME/deploy.log
+else
+  echo "> kill -15 $CURRENT_PID"
+  kill -15 $CURRENT_PID
+  sleep 5
+fi
+
+DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
+echo "> DEPLOY_JAR 배포"    >> /home/ubuntu/$PROJECT_NAME/deploy.log
+nohup java -jar $DEPLOY_JAR >> /home/ubuntu/$PROJECT_NAME/deploy.log 2>/home/ubuntu/$PROJECT_NAME/deploy_err.log &
