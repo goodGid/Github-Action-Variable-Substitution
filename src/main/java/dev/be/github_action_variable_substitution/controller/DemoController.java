b1d8b324e86b71b99f0a7c850bbc3950b12dfa97
goodGid <hello.goodgid@gmail.com>
2022-01-31 10:24:42 +0900

    Update DemoController

--- src/main/java/dev/be/github_action_variable_substitution/controller/DemoController.java
+++ src/main/java/dev/be/github_action_variable_substitution/controller/DemoController.java
@@ -7,11 +7,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoController {
 
-    @Value("${application.value}")
-    private String value;
+    /*
+    Github Action에서 주입받은 값으로 변경됨을 확인하기 위한 변수
+     */
+    @Value("${override.value}")
+    private String overrideValue;
+
+    /*f
+    프로젝트 application.yaml에 정의되어 있지 않지만
+    Github Action에서 주입 받아 사용이 가능함을 확인하기 위한 변수
+     */
+    @Value("${from.github.secret:null}")
+    private String fromGithubSecret;
 
     @GetMapping("/demo")
     public String demo() {
-        return value;
+        return overrideValue + " / " + fromGithubSecret;
     }
 }
