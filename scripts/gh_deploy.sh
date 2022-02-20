815cca360e8a1f97b895a904fe732835555e60ca
Kiyong Shin <hello.goodgid@gmail.com>
2022-02-20 21:12:19 +0900

    Update gh_deploy.sh

--- scripts/gh_deploy.sh
+++ scripts/gh_deploy.sh
@@ -14,12 +14,12 @@ echo "> build 파일명: $JAR_NAME" >> $DEPLOY_LOG_PATH
 echo "> build 파일 복사" >> $DEPLOY_LOG_PATH
 cp $BUILD_JAR $DEPLOY_PATH
 
-echo "> 현재 동작하는 어플리케이션 pid 체크" >> $DEPLOY_LOG_PATH
+echo "> 현재 동작중인 어플리케이션 pid 체크" >> $DEPLOY_LOG_PATH
 CURRENT_PID=$(pgrep -f $JAR_NAME)
 
 if [ -z $CURRENT_PID ]
 then
-  echo "> 현재 동작하는 어플리케이션 존재 X" >> $DEPLOY_LOG_PATH
+  echo "> 현재 동작중인 어플리케이션 존재 X" >> $DEPLOY_LOG_PATH
 else
   echo "> 현재 동작중인 어플리케이션 존재 O" >> $DEPLOY_LOG_PATH
   echo "> 현재 동작중인 어플리케이션 강제 종료 진행" >> $DEPLOY_LOG_PATH
