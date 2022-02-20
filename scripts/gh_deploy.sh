6085c8d89c02bc5e9126cdef24122dbfcc2b0e3b
Kiyong Shin <hello.goodgid@gmail.com>
2022-02-20 21:04:05 +0900

    Update gh_deploy.sh

--- scripts/gh_deploy.sh
+++ scripts/gh_deploy.sh
@@ -21,13 +21,14 @@ if [ -z $CURRENT_PID ]
 then
   echo "> 현재 동작하는 어플리케이션 존재 X" >> $DEPLOY_LOG_PATH
 else
-  echo "> 현재 구동중인 애플리케이션이 있으므로 강제 종료" >> /home/ubuntu/$PROJECT_NAME/action/deploy.log
+  echo "> 현재 동작중인 어플리케이션 존재 O" >> $DEPLOY_LOG_PATH
+  echo "> 현재 동작중인 어플리케이션 강제 종료 진행" >> $DEPLOY_LOG_PATH
   echo "> kill -9 $CURRENT_PID"
   kill -9 $CURRENT_PID
 fi
 
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
 echo "> DEPLOY_JAR 배포" >> $DEPLOY_LOG_PATH
-nohup java -jar --server.port=8081 -Dspring.profiles.active=local $DEPLOY_JAR  >> $DEPLOY_LOG_PATH 2> $DEPLOY_ERR_LOG_PATH &
+nohup java -jar -Dspring.profiles.active=local $DEPLOY_JAR --server.port=8081 >> $DEPLOY_LOG_PATH 2> $DEPLOY_ERR_LOG_PATH &
 
 echo "> 배포 종료" >> $DEPLOY_LOG_PATH
