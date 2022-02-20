92423b6f6dff9c79e1183eaa0b64e0db7da402c9
Kiyong Shin <hello.goodgid@gmail.com>
2022-02-20 20:59:12 +0900

    Update gh_deploy.sh

--- scripts/gh_deploy.sh
+++ scripts/gh_deploy.sh
@@ -6,9 +6,11 @@ DEPLOY_LOG_PATH="/home/ubuntu/$PROJECT_NAME/application.log"
 DEPLOY_ERR_LOG_PATH="/home/ubuntu/$PROJECT_NAME/deploy_err.log"
 BUILD_JAR=$(ls $JAR_PATH)
 JAR_NAME=$(basename $BUILD_JAR)
+now=$(date)
+
+echo "> 배포 시작 : $now" >> /home/ubuntu/$PROJECT_NAME/action/deploy.log
 
 echo "> build 파일명: $JAR_NAME" >> $DEPLOY_LOG_PATH
-
 echo "> build 파일 복사" >> $DEPLOY_LOG_PATH
 cp $BUILD_JAR $DEPLOY_PATH
 
@@ -19,11 +21,13 @@ if [ -z $CURRENT_PID ]
 then
   echo "> 현재 동작하는 어플리케이션 존재 X" >> $DEPLOY_LOG_PATH
 else
-  echo "> 기존에 동작하는 어플리케이션 강제 종료 : kill -15 $CURRENT_PID" >> $DEPLOY_LOG_PATH
-  kill -15 $CURRENT_PID
-  sleep 5
+  echo "> 현재 구동중인 애플리케이션이 있으므로 강제 종료" >> /home/ubuntu/$PROJECT_NAME/action/deploy.log
+  echo "> kill -9 $CURRENT_PID"
+  kill -9 $CURRENT_PID
 fi
 
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
 echo "> DEPLOY_JAR 배포" >> $DEPLOY_LOG_PATH
-nohup java -jar $DEPLOY_JAR --server.port=8081 >> $DEPLOY_LOG_PATH 2> $DEPLOY_ERR_LOG_PATH &
+nohup java -jar --server.port=8081 -Dspring.profiles.active=local $DEPLOY_JAR  >> $DEPLOY_LOG_PATH 2> $DEPLOY_ERR_LOG_PATH &
+
+echo "> 배포 종료" >> /home/ubuntu/$PROJECT_NAME/action/deploy.log
