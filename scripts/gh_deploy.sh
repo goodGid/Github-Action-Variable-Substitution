b824d3a34d42f675706813c0f3887e95f829a3bd
Kiyong Shin <hello.goodgid@gmail.com>
2022-02-20 21:27:04 +0900

    Update gh_deploy.sh

--- scripts/gh_deploy.sh
+++ scripts/gh_deploy.sh
@@ -31,4 +31,6 @@ DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
 echo "> DEPLOY_JAR 배포" >> $DEPLOY_LOG_PATH
 nohup java -jar -Dspring.profiles.active=local $DEPLOY_JAR --server.port=8081 >> $APPLICATION_LOG_PATH 2> $DEPLOY_ERR_LOG_PATH &
 
+sleep 3
+
 echo "> 배포 종료 : $(date +%c)" >> $DEPLOY_LOG_PATH
